7905ea8cb309830b4cfc65a8821c8dffe7f9d368
Probably Raging <probablyraging@gmail.com>
2023-01-04 10:39:33 +1100

    Refactored existing code Added removal of Shorts navigation button, Shorts channel tab, and the Shorts shelf on the home page

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,4 +1,6 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // Listen for updates to the current tab and if the tab's URL
+    // includes "https://www.youtube.com/", execute the hideShorts function
     if (tab.url.includes("https://www.youtube.com/")) {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
@@ -8,14 +10,42 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 });
 
 function hideShorts() {
+    // Check for shorts videos and tabs every 100 milliseconds
     setInterval(() => {
         chrome.storage.sync.get(['toggleState'], function (result) {
             if (result.toggleState === 'on') {
-                const shorts = document.querySelectorAll('[href^="/shorts/"]');
-                shorts.forEach(short => {
-                    short.parentNode.parentNode.parentNode.style.display = 'none';
-                });
+                // If the toggle state is set to "on", hide the shorts videos, menu tabs, and shelf
+                hideElements('.tab-content', 'shorts');
+                hideElements('[href^="/shorts/"]');
+                hideElements('#endpoint[title="Shorts"]');
+                hideElements('#title.style-scope.ytd-rich-shelf-renderer');
             }
         });
     }, 100);
-};
+
+    function hideElements(selector, text) {
+        // Find all elements matching the provided selector and hide them if they contain the provided text
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(element => {
+            if (!text || element.textContent.toLowerCase().replace(/\s/g, '').replace(/\n/g, '') === text) {
+                if (selector === '#title.style-scope.ytd-rich-shelf-renderer') {
+                    // Remove 5 parent nodes for the shorts shelf element
+                    removeParentNodes(element, 5);
+                } else {
+                    const parent = element.parentNode;
+                    parent.parentNode.removeChild(parent);
+                }
+            }
+        });
+    }
+
+    function removeParentNodes(element, n) {
+        for (let i = 0; i < n; i++) {
+            element = element.parentNode;
+            if (!element) {
+                return;
+            }
+        }
+        element.parentNode.removeChild(element);
+    }
+}
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,8 +1,9 @@
-// When popup window is opened
-window.addEventListener('load', checkState);
-function checkState() {
-    // Fetch toggle state from storage
-    chrome.storage.sync.get(['toggleState'], function (result) {
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const reloadBtn = document.getElementById('reloadBtn');
+
+    // When popup window is opened, check the toggle state and update the UI accordingly
+    chrome.storage.sync.get(['toggleState'], result => {
         if (result.toggleState === undefined) {
             // Update toggle state in storage
             chrome.storage.sync.set({ toggleState: 'on' });
@@ -10,36 +11,30 @@ function checkState() {
             toggleBtn.src = '../images/power-button-on.svg';
         } else {
             toggleBtn.src = '../images/power-button-off.svg';
-        }
-    });
-}
-
-// When toggle button is clicked
-const toggleBtn = document.getElementById('toggleBtn');
-toggleBtn.addEventListener('click', toggleClick);
-function toggleClick() {
-    // Fetch toggle state from storage
-    chrome.storage.sync.get(['toggleState'], function (result) {
-        // Toggle extension state on or off and update storage key
-        if (result.toggleState === 'on') {
-            toggleBtn.src = '../images/power-button-off.svg';
-            chrome.storage.sync.set({ toggleState: 'off' });
             reloadBtn.style.display = 'block';
-        } else {
-            toggleBtn.src = '../images/power-button-on.svg';
-            chrome.storage.sync.set({ toggleState: 'on' });
-            reloadBtn.style.display = 'none';
         }
     });
-}
 
-// When reload button is clicked
-const reloadBtn = document.getElementById('reloadBtn');
-reloadBtn.addEventListener('click', reloadClick);
-function reloadClick() {
-    // Find active tab and reload it
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
+    // When toggle button is clicked, toggle the extension state and update the storage key
+    toggleBtn.addEventListener('click', () => {
+        chrome.storage.sync.get(['toggleState'], result => {
+            if (result.toggleState === 'on') {
+                toggleBtn.src = '../images/power-button-off.svg';
+                chrome.storage.sync.set({ toggleState: 'off' });
+                reloadBtn.style.display = 'block';
+            } else {
+                toggleBtn.src = '../images/power-button-on.svg';
+                chrome.storage.sync.set({ toggleState: 'on' });
+                reloadBtn.style.display = 'none';
+            }
+        });
     });
-    window.close();
-}
+
+    // When reload button is clicked, find the active tab and reload it
+    reloadBtn.addEventListener('click', () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
+        });
+        window.close();
+    });
+});
