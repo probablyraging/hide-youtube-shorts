8eaaae5b7ffd60477b18af9684cc057075ea2644
ProbablyRaging <probablyraging@gmail.com>
2023-06-24 22:02:15 +1000

    Updated manifest version

--- dist/assets/MainPage.js
+++ dist/assets/MainPage.js
@@ -1,7 +1,1 @@
-import { j as jsxRuntimeExports } from "./index.js";
-const MainPage = ({ children }) => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "flex flex-col max-h-[518px] max-w-[450px] overflow-scroll p-4 pt-0", children });
-};
-export {
-  MainPage as default
-};
+import{j as a}from"./index.js";const o=({children:x})=>a.jsx("div",{className:"flex flex-col max-h-[518px] max-w-[450px] overflow-scroll p-4 pt-0",children:x});export{o as default};
